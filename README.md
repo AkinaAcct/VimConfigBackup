def8b208246f4f490804ebd6409faf9f05794564
nya-sec <nyaisacat@gmail.com>
2023-05-23 22:39:57 +0800

    add fish config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Just a backup of my [Vim](https://github.com/vim/vim), [NeoVim](https://github.com/neovim/neovim) and [Termux](https://github.com/termux/termux-app) extra-keys configs
+# ~~Just a backup of my [Vim](https://github.com/vim/vim), [NeoVim](https://github.com/neovim/neovim) and [Termux](https://github.com/termux/termux-app) extra-keys configs~~
+
+# 这里已经成了我的配置备份库了qwq
---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+function fish_greeting
+end
+alias g "git"
+alias ga "git add"
+alias gaa "git add --all"
+alias gc "git commit --verbose"
+alias gp "git push"
+alias gs "git status"
+alias gt "git tag"
+alias gd "git describe"
+alias gdt "gd --tags"
+alias gdo "onefetch"
+alias vim "nvim"
+alias cat "bat -pp"
+alias ls "exa --color=auto"
+alias la "ls -a"
+alias ll "ls -al"
+alias llh "ls -alh"
